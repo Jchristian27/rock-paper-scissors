9b72d124faa3355c141bcb206773e6e97ff1989c
Jonathan Christian <christianj707@gmail.com>
2022-08-31 14:22:52 -0700

    Create game() function to play 5 rounds and track score

--- index.js
+++ index.js
@@ -5,33 +5,54 @@ function getRndInteger(min, max) {
 function getComputerChoice() {
   options = ["rock", "paper", "scissors"];
   choice = options[getRndInteger(1,4) - 1];
-  return choice
+  return choice;
 }
 
 function singleRound(computerSelection, playerSelection) {
   computerSelection = getComputerChoice();
   playerSelection = window.prompt("Rock, Paper, or Scissors?").toLowerCase();
   if (computerSelection == "rock" && playerSelection == "rock") {
-    console.log("It's a tie!")
+    console.log(`It's a tie! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "rock" && playerSelection == "paper") {
-    console.log("You win! Paper beats rock!")
+    playerScore += 1
+    console.log(`You win! Paper beats rock! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    console.log ("You lose! Rock beats scissors!")
+    computerScore += 1
+    console.log (`You lose! Rock beats scissors! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "paper" && playerSelection == "paper") {
-    console.log("It's a tie!")
+    console.log(`It's a tie! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "paper" && playerSelection == "rock") {
-    console.log("You lose! Paper beats rock!")
+    computerScore += 1
+    console.log(`You lose! Paper beats rock! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "paper" && playerSelection == "scissors") {
-    console.log ("You win! Scissors beats paper!")
+    playerScore += 1
+    console.log (`You win! Scissors beats paper! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "scissors" && playerSelection == "scissors") {
-    console.log("It's a tie!")
+    console.log(`It's a tie! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    console.log("You lose! Scissors beats paper!")
+    computerScore += 1
+    console.log(`You lose! Scissors beats paper! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else if (computerSelection == "scissors" && playerSelection == "rock") {
-    console.log ("You win! Rock beats scissors!")
+    playerScore += 1
+    console.log (`You win! Rock beats scissors! Player Score: ${playerScore} Computer Score: ${computerScore}`)
   } else {
     console.log("Uh oh. Something went wrong!")
   }
 }
 
-singleRound()
+function game() {
+  computerScore = 0;
+  playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+     if (i < 5) {
+      singleRound()
+    }
+  }
+  if (computerScore == 5) {
+    console.log("The Computer Won :(")
+  } else if (playerScore == 5) {
+    console.log("You won!")
+  } 
+}
+
+game()
