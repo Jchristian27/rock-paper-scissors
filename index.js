12ef53eb9a66ae81ab09c66b7b7c05c5287b0f11
Jonathan Christian <christianj707@gmail.com>
2022-08-31 13:28:30 -0700

    Create a function to plat a single rounf

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ function getRndInteger(min, max) {
 }
 
 function getComputerChoice() {
-  options = ["Rock", "Paper", "Scissors"]
-  choice = options[getRndInteger(1,4) - 1]
-  console.log(choice)
+  options = ["rock", "paper", "scissors"];
+  choice = options[getRndInteger(1,4) - 1];
+  return choice
 }
 
-getComputerChoice()
+function singleRound(computerSelection, playerSelection) {
+  computerSelection = getComputerChoice();
+  playerSelection = window.prompt("Rock, Paper, or Scissors?");
+  console.log(computerSelection)
+  console.log(playerSelection)
+  if (computerSelection == "rock" && playerSelection == "rock") {
+    console.log("It's a tie!")
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    console.log("You win! Paper beats rock!")
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    console.log ("You lose! Rock beats scissors!")
+  } else if (computerSelection == "paper" && playerSelection == "paper") {
+    console.log("It's a tie!")
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    console.log("You lose! Paper beats rock!")
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    console.log ("You win! Scissors beats paper!")
+  } else if (computerSelection == "scissors" && playerSelection == "scissors") {
+    console.log("It's a tie!")
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    console.log("You lose! Scissors beats paper!")
+  } else if (computerSelection == "scissors" && playerSelection == "rock") {
+    console.log ("You win! Rock beats scissors!")
+  } else {
+    console.log("Uh oh. Something went wrong!")
+  }
+}
+
+singleRound()
