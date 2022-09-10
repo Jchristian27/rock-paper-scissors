5c21160b8fd2ef05d57f1c7144c9cd7b27bf3a5a
Jonathan Christian <christianj707@gmail.com>
2022-09-10 11:28:46 -0700

    Clean up JS file

--- index.js
+++ index.js
@@ -18,6 +18,29 @@ function declareWinner() {
   }
 }
 
+function game(computerSelection, playerSelection) {
+  winner = "";
+  if (computerSelection === "rock" && playerSelection === "paper") {
+    playerScore += 1;
+    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
+    playerScoreHTML.innerText = playerScore;
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    playerScore += 1;
+    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
+    playerScoreHTML.innerText = playerScore;
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    playerScore += 1;
+    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
+    playerScoreHTML.innerText = playerScore;
+  } else if (computerSelection === playerSelection) {
+    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>It's a tie!</h3>`;
+  } else {
+    computerScore += 1
+    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You lose!</h3>`;
+    computerScoreHTML.innerText = computerScore;
+  }
+}
+
 let choiceRock = document.querySelector('#rock');
 let choicePaper = document.querySelector('#paper');
 let choiceScissors = document.querySelector('#scissors');
@@ -42,26 +65,3 @@ choiceScissors.addEventListener('click', function() {
   game(getComputerChoice(), 'scissors');
   declareWinner();
 });
-
-function game(computerSelection, playerSelection) {
-  winner = "";
-  if (computerSelection === "rock" && playerSelection === "paper") {
-    playerScore += 1;
-    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = playerScore;
-  } else if (computerSelection === "paper" && playerSelection === "scissors") {
-    playerScore += 1;
-    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = playerScore;
-  } else if (computerSelection === "scissors" && playerSelection === "rock") {
-    playerScore += 1;
-    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = playerScore;
-  } else if (computerSelection === playerSelection) {
-    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>It's a tie!</h3>`;
-  } else {
-    computerScore += 1
-    title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}</h2><h3>You lose!</h3>`;
-    computerScoreHTML.innerText = computerScore;
-  }
-}
