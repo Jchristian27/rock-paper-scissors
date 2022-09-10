93f465602e735ee503b39d9581e98f91465da5b9
Jonathan Christian <christianj707@gmail.com>
2022-09-10 11:07:02 -0700

    Finish project up

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let defaultDomState = document.body.innerHTML;
+
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
@@ -8,26 +10,39 @@ function getComputerChoice() {
   return choice;
 }
 
+function declareWinner() {
+  if (playerScore > 4) {
+    window.alert("You Won!");
+    document.location.reload()
+  } else if (computerScore > 4) {
+    window.alert("You Lost :(");
+    document.location.reload()
+  }
+}
+
 let choiceRock = document.querySelector('#rock');
 let choicePaper = document.querySelector('#paper');
 let choiceScissors = document.querySelector('#scissors');
 let title = document.querySelector('.title');
-let playerScoreHTML = document.querySelector('#player-score');
-let computerScoreHTML = document.querySelector('#computer-score');
+let playerScoreHTML = document.querySelector('#player-score-num');
+let computerScoreHTML = document.querySelector('#computer-score-num');
 let playerScore = 0;
 let computerScore = 0;
 
 
-choiceRock.addEventListener('mousedown', function() {
+choiceRock.addEventListener('click', function() {
   game(getComputerChoice(), 'rock');
+  declareWinner();
 });
 
-choicePaper.addEventListener('mousedown', function() {
+choicePaper.addEventListener('click', function() {
   game(getComputerChoice(), 'paper');
+  declareWinner();
 });
 
-choiceScissors.addEventListener('mousedown', function() {
+choiceScissors.addEventListener('click', function() {
   game(getComputerChoice(), 'scissors');
+  declareWinner();
 });
 
 function game(computerSelection, playerSelection) {
@@ -35,24 +50,26 @@ function game(computerSelection, playerSelection) {
   if (computerSelection === "rock" && playerSelection === "paper") {
     playerScore += 1;
     title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}.</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = `Player Score: ${playerScore}`;
+    playerScoreHTML.innerText = playerScore;
   } else if (computerSelection === "paper" && playerSelection === "scissors") {
     playerScore += 1;
     title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}.</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = `Player Score: ${playerScore}`;
+    playerScoreHTML.innerText = playerScore;
   } else if (computerSelection === "scissors" && playerSelection === "rock") {
     playerScore += 1;
     title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}.</h2><h3>You win!</h3>`;
-    playerScoreHTML.innerText = `Player Score: ${playerScore}`;
+    playerScoreHTML.innerText = playerScore;
   } else if (computerSelection === playerSelection) {
     title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}.</h2><h3>It's a tie!</h3>`;
   } else {
     computerScore += 1
     title.innerHTML = `<h2>You chose ${playerSelection} and the computer chose ${computerSelection}.</h2><h3>You lose!</h3>`;
-    computerScoreHTML.innerText = `Computer Score: ${computerScore}`;
+    computerScoreHTML.innerText = computerScore;
   }
 }
 
+//When you need to reset the DOM, just replace the current body with:
+// document.body.innerHTML = defaultDomState;
 
 // function game() {
 //   computerScore = 0;
